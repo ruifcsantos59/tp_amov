aee0f276c5fb9f78a06b74bc7301a0ca1aabd90f
jsantos97 <45185544+jsantos97@users.noreply.github.com>
2018-12-03 18:59:21 +0000

    v3

--- app/src/main/java/pt/a21260411a21260403isec/tp_amov/MainActivity.java
+++ app/src/main/java/pt/a21260411a21260403isec/tp_amov/MainActivity.java
@@ -1,7 +1,9 @@
 package pt.a21260411a21260403isec.tp_amov;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void umJogador(View v) {
+        Intent intent = new Intent(this,onePlayer.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/pt/a21260411a21260403isec/tp_amov/onePlayer.java
@@ -0,0 +1,13 @@
+package pt.a21260411a21260403isec.tp_amov;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class onePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_player);
+    }
+}
