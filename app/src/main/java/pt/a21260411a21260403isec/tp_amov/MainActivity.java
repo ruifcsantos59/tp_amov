841ee6aa19e3145a477efe0c7357a7126c938c93
Rui Santos <ruifcsantos59@gmail.com>
2018-12-03 16:37:05 +0000

    v1

---
+++ app/src/main/java/pt/a21260411a21260403isec/tp_amov/MainActivity.java
@@ -0,0 +1,13 @@
+package pt.a21260411a21260403isec.tp_amov;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
