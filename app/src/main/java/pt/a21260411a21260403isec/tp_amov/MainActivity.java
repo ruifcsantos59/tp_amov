a1371370e0c5a03af8593e3000f7515c054cb95c
jsantos97 <45185544+jsantos97@users.noreply.github.com>
2018-12-03 19:35:03 +0000

    v5

--- app/src/main/java/pt/a21260411a21260403isec/tp_amov/MainActivity.java
+++ app/src/main/java/pt/a21260411a21260403isec/tp_amov/MainActivity.java
@@ -2,8 +2,10 @@ package pt.a21260411a21260403isec.tp_amov;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +13,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface font = Typeface.createFromAsset(getAssets(),"fonts/SMARC___.TTF");
+        ((TextView)findViewById(R.id.Title)).setTypeface(font);
+
     }
 
     public void umJogador(View v) {
