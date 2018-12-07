c7d8538490bd9f9e5ef84235ab90085b763f20b3
Rui Santos <ruifcsantos59@gmail.com>
2018-12-07 15:15:21 +0000

    v1

--- app/src/main/java/pt/a21260411a21260403isec/tp_amov/MainActivity.java
+++ app/src/main/java/pt/a21260411a21260403isec/tp_amov/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //User
         Typeface font = Typeface.createFromAsset(getAssets(),"fonts/SMARC___.TTF");
         ((TextView)findViewById(R.id.Title)).setTypeface(font);
 
